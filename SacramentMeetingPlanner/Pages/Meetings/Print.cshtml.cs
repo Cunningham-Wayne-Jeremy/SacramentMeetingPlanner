1e0b5248523601d63b1ee2dd5cb90bd59237fe0d
Cristiano Trindade <cristiano.trindade@me.com>
2021-04-05 13:28:19 -0700

    Cris's Print Changes

---
+++ SacramentMeetingPlanner/Pages/Meetings/Print.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SacramentMeetingPlanner.Data;
+using SacramentMeetingPlanner.Models;
+
+namespace SacramentMeetingPlanner.Pages.Meetings
+{
+    public class PrintModel : PageModel
+    {
+        private readonly SacramentMeetingPlanner.Data.SacramentMeetingPlannerContext _context;
+
+        public PrintModel(SacramentMeetingPlanner.Data.SacramentMeetingPlannerContext context)
+        {
+            _context = context;
+        }
+
+        public Meeting Meeting { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Meeting = await _context.Meeting.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Meeting == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+    }
+}
